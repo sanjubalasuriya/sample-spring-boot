1f62f55f0ddd099a7c5c5d4b39248bc6f1b818cf
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-05 23:58:39 +0530

    Update Order Controller

--- src/main/java/com/sample/controller/OrderController.java
+++ src/main/java/com/sample/controller/OrderController.java
@@ -1,16 +1,14 @@
 package com.sample.controller;
 
-import com.sample.dto.request.RequestItemDTO;
+
+import com.sample.dto.paginated.ResponseOrderDetailsPaginatedDTO;
 import com.sample.dto.request.RequestOrderSaveDTO;
 import com.sample.service.OrderService;
 import com.sample.util.StandardResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -26,5 +24,19 @@ public class OrderController {
 
         return new ResponseEntity<StandardResponse>(new StandardResponse(201, "success", message)
                 , HttpStatus.CREATED);
+    }
+
+    @GetMapping(
+            path = "/paginate-all",
+            params = {"page", "size"}
+    )
+    public ResponseEntity<StandardResponse> getAllIOrderDetails(
+            @RequestParam(value = "page") int page,
+            @RequestParam(value = "size") int size
+    ) {
+        ResponseOrderDetailsPaginatedDTO responseOrderDetailsPaginatedDTO = orderService.getAllIOrderDetails(page, size);
+//        List<CustomerDTO> allCustomers = customerService.getAllCustomersPaginate(page, size);
+        return new ResponseEntity<StandardResponse>(new StandardResponse(200, "success", responseOrderDetailsPaginatedDTO)
+                , HttpStatus.OK);
     }
 }
