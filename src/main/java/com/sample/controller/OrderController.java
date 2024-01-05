9ada6c29a95ded8f2f7d9fc26cfe2aa78c51a0dc
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-05 21:50:30 +0530

    Update OrderController

--- src/main/java/com/sample/controller/OrderController.java
+++ src/main/java/com/sample/controller/OrderController.java
@@ -1,4 +1,30 @@
 package com.sample.controller;
 
+import com.sample.dto.request.RequestItemDTO;
+import com.sample.dto.request.RequestOrderSaveDTO;
+import com.sample.service.OrderService;
+import com.sample.util.StandardResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/order")
+@AllArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/save")
+    public ResponseEntity<StandardResponse> saveOrder(@RequestBody RequestOrderSaveDTO requestOrderSaveDTO) {
+
+        String message = orderService.saveOrder(requestOrderSaveDTO);
+
+        return new ResponseEntity<StandardResponse>(new StandardResponse(201, "success", message)
+                , HttpStatus.CREATED);
+    }
 }
