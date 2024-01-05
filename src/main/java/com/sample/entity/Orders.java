9079f87ef321cda2c79cee69cbd140751c3a50a6
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-05 21:50:52 +0530

    Update Orders

--- src/main/java/com/sample/entity/Orders.java
+++ src/main/java/com/sample/entity/Orders.java
@@ -1,22 +1,47 @@
 package com.sample.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "orders")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class Orders {
 
     @Id
+    @Column(name = "orders_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int orderId;
+
+    private Double total;
 
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
+
+    @OneToMany(mappedBy = "orders")
+    private Set<OrderDetails> orderDetails;
+
+    private Date created_At;
+    private Date updated_At;
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
+    public Orders(Double total, Customer customer) {
+        this.total = total;
+        this.customer = customer;
+    }
 }
