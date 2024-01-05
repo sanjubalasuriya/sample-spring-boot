1fc373ad340d70c5e37468b1e3dd5f9ca5b0b820
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-05 21:50:12 +0530

    Update Item

--- src/main/java/com/sample/entity/Item.java
+++ src/main/java/com/sample/entity/Item.java
@@ -1,15 +1,17 @@
 package com.sample.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "item")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class Item {
 
     @Id
@@ -19,9 +21,25 @@ public class Item {
     private int qty;
     private double unitPrice;
 
+    private Date created_At;
+    private Date updated_At;
+
+    @OneToMany(mappedBy = "item")
+    private Set<OrderDetails> orderDetails;
+
     public Item(String name, int qty, double unitPrice) {
         this.name = name;
         this.qty = qty;
         this.unitPrice = unitPrice;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
     }
 }
